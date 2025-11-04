03f8e4c79c548127eaa5715c692df2d8cdbc0c44
Benjamin Milan <benjamin.milan@sky.uk>
2025-11-04 11:40:06 +0000

    Added base branch functions

--- git_flow_library/git_flow_library.py
+++ git_flow_library/git_flow_library.py
@@ -118,23 +118,79 @@ class GitFlowLibrary:
         subprocess.run(cmd, cwd=directory, check=True)
 
     @staticmethod
-    def get_master_branch(directory: str | Path) -> str:
-        if not GitFlowLibrary.is_gitflow_enabled(directory):
-            raise RuntimeError("Tried to get gitflow branch but gitflow not enabled!")
-        return subprocess.run(
-            ['git', 'config', 'gitflow.branch.master'],
-            cwd=directory, 
-            capture_output=True, 
-            text=True
-        ).stdout.strip()
+    def get_master_branch(directory: str | Path) -> str | None:
+        branch = GitFlowLibrary.get_config_value('branch.master', directory)
+        if not branch:
+            raise RuntimeError(
+                "Master branch not set in gitflow config. This shouldn't be possible.")
+        return branch
     
     @staticmethod
-    def get_develop_branch(directory: str | Path) -> str:
+    def get_develop_branch(directory: str | Path) -> str | None:
+        branch = GitFlowLibrary.get_config_value('branch.develop', directory)
+        if not branch:
+            raise RuntimeError(
+                "Develop branch not set in gitflow config. This shouldn't be possible.")
+        return branch
+    
+    @staticmethod
+    def get_branch_base(branch: str, directory: str | Path) -> str | None:
+        """Get the base branch for a given gitflow branch.
+
+        Args:
+            branch (str): The branch to find the base of.
+            directory (str | Path): Path to repository directory.
+
+        Returns:
+            str | None: The base branch name if set, otherwise None.
+        """        
+        return GitFlowLibrary.get_config_value(f'branch.{branch}.base', directory)
+    
+    @staticmethod
+    def set_branch_base(branch: str, base: str, directory: str | Path):
+        """Set the base branch for a given gitflow branch.
+
+        Args:
+            branch (str): The branch to configure.
+            base (str): The base branch name to associate.
+            directory (str | Path): Path to the repository directory.
+
+        Raises:
+            RuntimeError: If gitflow not enabled.
+        """        
         if not GitFlowLibrary.is_gitflow_enabled(directory):
-            raise RuntimeError("Tried to get gitflow branch but gitflow not enabled!")
-        return subprocess.run(
-            ['git', 'config', 'gitflow.branch.develop'],
+            raise RuntimeError(
+                f"Tried to set gitflow branch base in {directory} but gitflow "
+                "not enabled!"
+            )
+        subprocess.run(
+            ['git', 'flow', 'config', 'base', '--set', branch, base],
+            cwd=directory
+        )
+    
+    @staticmethod
+    def get_config_value(key: str, directory: str | Path) -> str | None: 
+        """Get a gitflow config value for a repo in given directory.
+
+        Args:
+            key (str): The gitflow config key to retrieve (e.g. 'branch.master')
+            directory (str | Path): Path to repository directory.
+
+        Raises:
+            RuntimeError: If gitflow is not enabled in the specified directory.
+
+        Returns:
+            str | None: The configuration value if set, otherwise None.
+        """        
+        if not GitFlowLibrary.is_gitflow_enabled(directory):
+            raise RuntimeError(
+                f"Tried to read gitflow config in {directory} but gitflow "
+                "not enabled!"
+            )
+        out = subprocess.run(
+            ['git', 'config', f'gitflow.{key}'],
             cwd=directory,
             capture_output=True,
             text=True
         ).stdout.strip()
+        return out if out else None
